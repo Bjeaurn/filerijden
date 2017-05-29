d1f25dea66431c17e842b6ba449bfc4b1341a78f
Bjorn S <bjorn@somnu.com>
2017-05-29 18:50:43 +0200

    Initial commit, new boilerplate.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,19 @@ import { RouterModule, Routes } from '@angular/router'
 import { SharedModule } from './shared/shared.module'
 import { HttpClient } from './shared/http.client'
 import { BaseModule } from './base/base.module'
+import { HomeComponent } from './base/home.component'
 import { PageNotFoundComponent } from "./shared/pagenotfound.component"
 
 const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     PageNotFoundComponent
   ],
   imports: [
