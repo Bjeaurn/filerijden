682bd7eae02c797262333edacf8398c1887fbfa8
Bjorn S <bjorn@somnu.com>
2017-07-06 19:59:06 +0200

    Uses RDW api through CURL

--- src/api/includes/controllers/car.php
+++ src/api/includes/controllers/car.php
@@ -1,7 +1,16 @@
 <?php
 $kenteken = $routing[1];
 
-$response = file_get_contents("https://opendata.rdw.nl/resource/m9d7-ebf2.json?kenteken=".$kenteken);
+$url = 'https://opendata.rdw.nl/resource/m9d7-ebf2.json?$$app_token=siAJatNuW8SLBtzE04JSkFmWs&kenteken='.$kenteken;
+// $url = str_replace("&amp;", '&', $url);
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch,CURLOPT_USERAGENT,"Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+curl_close($ch);
+
 $response = json_decode($response);
 $resObj = $response[0];
 
